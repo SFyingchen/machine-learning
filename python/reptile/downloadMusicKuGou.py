3f9d9a1df24a47e439aea683e17a67c1688166a8
unknown <253470635@qq.com>
2024-06-23 23:45:04 +0800

    aa

--- python/reptile/downloadMusicKuGou.py
+++ python/reptile/downloadMusicKuGou.py
@@ -4,3 +4,5 @@
 # @Time : 2024/6/14 16:46
 # downloadMusicKuGou:  python 项目练习--爬虫   获取酷狗音乐
 url = "https://webfs.hw.kugou.com/202406141648/276287f31ae5e297df733f2333db56c4/v2/0ec0dfe7a4090b3c5c2c1831d7f3ed51/part/0/960127/G185/M08/17/0B/clip_-Q0DAF4WEP6AbxIRADmMOR-Oo88467.mp3"
+import requests
+
